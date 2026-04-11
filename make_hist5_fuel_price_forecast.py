e276691a41f45561d78fd247fe2acaf58f2ffe2e
Matthias Fripp <matthias@energyinnovation.org>
2026-04-10 17:38:36 -0700

    Improve clustering of generators and produce history-based forecasts for all fuels
    This commit does two main things:
    
      - creates broader aggregation groups for generators to capture almost all
        existing generators and make sure they use the right fuel
      - updates the history-based forecasts to cover all fuels and also produce
        prices for individual past years for back-testing
    
    Specifically, it does the following:
    
      - create distinct Petroleum Liquids generators instead of including them in
        the "Other_peaker" group; use distillate as the fuel for these (more common
        than fuel oil for these in the EIA generation data)
      - create a Waste Heat generator made up of "All Other" generators from EIA,
        previously omitted (mainly cogen at refineries)
      - Add the following previously omitted generators from EIA to the specified
        groups (all EIA generators are now included in the model except natural gas
        with CAES and solar thermal with energy storage)
        - Flywheels -> Batteries
        - Coal IGCC -> Steam Coal
        - Petroleum Coke -> Steam Coal
        - Gas Steam -> Other_peaker
        - Other Gases -> Other_peaker
        - Other Natural Gas -> Other_peaker
      - disable economic retirement for Biomass and Petroleum generators (we assume
        they have some reason for running)
      - increase existing coal and combined cycle gas generators from one cluster
        per region to two
      - create forecasts for distillate and uranium prices based on state-level
        historical prices, in addition to coal and natural gas previously handled
        this way
      - supplement state-level historical fuel prices from SEDS with additional data
        from EIA Electric Power Operations product when needed to fill in missing
        fuels or years

--- make_hist5_fuel_price_forecast.py
+++ make_hist5_fuel_price_forecast.py
@@ -75,18 +75,57 @@ eia_api_key = eia_api_key_file.read_text().strip()
 
 ################
 # Retrieve EIA historical prices
+
 # See SEDS query constructor at https://www.eia.gov/opendata/browser/seds to get example URL like
 # https://api.eia.gov/v2/seds/data/?frequency=annual&data[0]=value&facets[seriesId][]=CLEID&facets[seriesId][]=NGEID&start=2020&end=2024&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000
 
+# Also see EIA electric power operations (EPO) query builder at
+# https://www.eia.gov/opendata/browser/electricity/electric-power-operational-data
+# EIA electric power API generally has more recent data than SEDS (which has a ~18 mo lag)
+# but it lacks nuclear, which is in SEDS. 2023 and 2024 have similar
+# availability but state/sector/fuel coverage can be patchy, e.g.,
+#              Electric Utility Electric Power
+#                             | |
+# subbituminous coal          + -
+# coal, excluding waste coal  + -
+# all coal products           + +  EP = SEDS
+# petroleum coke              - -
+# refined coal                - -
+# natural gas                 + * TX, UT, not MO  EP =~ SEDS
+# natural gas & other gases   + -
+# distillate fuel oil         + -  EU = SEDS
+# petroleum liquids           + * TX, MO, not UT
+# petroleum                   + -
+# residual fuel oil           - -
+# nuclear                     - -
+
 base_seds_url = "https://api.eia.gov/v2/seds/data/"
+base_epod_url = (
+    "https://api.eia.gov/v2/electricity/electric-power-operational-data/data/"
+)
 
-series_fuels = {
-    "CLEID": "coal",  # coal price, electric power sector
-    "NGEID": "naturalgas",  # gas price, electric power sector
+# from SEDS query constructor, SERIESID filtered to
+# "price in the electric power sector"
+seds_fuels = {
+    "CLEID": "coal",
+    "NGEID": "naturalgas",
+    "DKEID": "distillate",
+    "NUEGD": "uranium",
 }
-print(f"Retrieving fuel prices for {start_year}-{end_year} from {base_seds_url}")
+# from EPO query constructor, FUELTYPEID filtered to a fuel from
+# the EPO table above
+# "price in the electric power sector"
+epod_fuels = {
+    "COW": "coal",
+    "NG": "naturalgas",
+    "DFO": "distillate",
+    "NUC": "uranium",
+}
+# possible sector listsings for power prices, in priority order
+epod_sectors = {98: "Electric Power", 1: "Electric Utility"}
 
-params = [
+print(f"Retrieving fuel prices for {start_year}-{end_year} from {base_seds_url}")
+seds_params = [
     ("api_key", eia_api_key),
     ("frequency", "annual"),
     ("data[]", "value"),
@@ -95,47 +134,124 @@ params = [
     ("sort[0][column]", "period"),
     ("sort[0][direction]", "asc"),
     ("length", "5000"),
-] + [("facets[seriesId][]", s) for s in series_fuels.keys()]
+] + [("facets[seriesId][]", s) for s in seds_fuels.keys()]
 
-resp = requests.get(base_seds_url, params=params)
+resp = requests.get(base_seds_url, params=seds_params)
 resp.raise_for_status()
 
 rows = resp.json()["response"]["data"]
-state_prices = pd.DataFrame(rows)
+
+seds_prices = (
+    pd.DataFrame(rows)
+    .rename(columns={"period": "year", "stateId": "st", "value": "price"})
+    .eval("price = price.astype('float64')")
+    .eval("year = year.astype('Int64')")
+    .eval("fuel = seriesId.map(@seds_fuels)")
+    .query("st != 'US' and price > 0")
+    .loc[:, ["year", "st", "fuel", "price"]]
+)
+
+# initialize state_prices dataframe
+state_prices = seds_prices.copy()
+
+# fill in with electric power or electric utility data from
+# EPO API where needed
+print(
+    f"Retrieving additional fuel prices for {start_year}-{end_year} from {base_epod_url}"
+)
+epod_params = (
+    [
+        ("api_key", eia_api_key),
+        ("frequency", "annual"),
+        ("data[0]", "cost-per-btu"),
+        ("start", str(start_year)),
+        ("end", str(end_year)),
+        ("sort[0][column]", "period"),
+        ("sort[0][direction]", "asc"),
+        ("length", "5000"),
+    ]
+    + [("facets[sectorid][]", s) for s in epod_sectors.keys()]
+    + [("facets[fueltypeid][]", f) for f in epod_fuels.keys()]
+)
+
+resp = requests.get(base_epod_url, params=epod_params)
+resp.raise_for_status()
+
+rows = resp.json()["response"]["data"]
 
 # convert strings to numbers
-state_prices["period"] = state_prices["period"].astype(int)
-state_prices["value"] = state_prices["value"].astype(float)
 # pick the right columns, rename some, drop US total and missing values
-state_prices = (
-    state_prices.loc[:, ["period", "stateId", "seriesId", "value"]]
-    .query("stateId != 'US' and value > 0")
-    .rename(columns={"period": "year", "stateId": "st"})
+epod_prices = (
+    pd.DataFrame(rows)
+    .rename(columns={"period": "year", "location": "st", "cost-per-btu": "price"})
+    .eval("price = price.astype('float64')")
+    .eval("year = year.astype('Int64')")
+    .eval("fuel = fueltypeid.map(@epod_fuels)")
+    .eval("sector = sectorid.astype('Int64').map(@epod_sectors)")
+    .query("st.isin(@state_prices['st']) and price > 0")
+    .loc[:, ["year", "st", "sector", "fuel", "price"]]
 )
-# lookup fuel name
-state_prices["fuel"] = state_prices["seriesId"].map(series_fuels)
+
+
+def add_missing(df_to, df_from):
+    key_cols = ["year", "st", "fuel"]
+    to_add = (
+        df_from.merge(
+            df_to[key_cols],
+            on=key_cols,
+            how="left",
+            indicator=True,
+        )
+        .query("_merge == 'left_only'")
+        .drop(columns="_merge")
+    )
+    result = pd.concat([df_to, to_add], ignore_index=True)[df_to.columns]
+    return result
+
+
+# add EPO prices to state prices if needed, preferring Electric Power sector if
+# available, otherwise Electric Utility sector
+
+for s in epod_sectors.values():
+    state_prices = add_missing(state_prices, epod_prices.query("sector == @s"))
+
+print(
+    f"Retrieved {len(seds_prices)} prices from SEDS and added {len(state_prices)-len(seds_prices)} from EPO."
+)
 
 # adjust for inflation
 cpi = get_cpi_data(start_year, end_year)
 inflator = (
     cpi.query("year == @base_year")["value"].iloc[0] / cpi.set_index("year")["value"]
 )
-state_prices["price_real"] = state_prices["value"] * state_prices["year"].map(inflator)
+state_prices["price_real"] = state_prices["price"] * state_prices["year"].map(inflator)
 
-# aggregate across years
-state_fcst = state_prices.groupby(["st", "fuel"])["price_real"].mean().reset_index()
+# make hist5 and peak forecasts
+fcst_info = [("hist5", state_prices["year"].unique())] + [
+    (f"y{y}", [y]) for y in range(start_year, end_year + 1)
+]
+fcst_dfs = []
+for fcst_name, fcst_base_years in fcst_info:
+    # aggregate across years
+    fcst = (
+        state_prices.query("year.isin(@fcst_base_years)")
+        .groupby(["st", "fuel"])["price_real"]
+        .mean()
+        .reset_index()
+    )
+    # for any states missing prices (defunct or absent plants),
+    # fill in with arbitrary 2x the national average (to avoid looking
+    # like they could be restarted cheaply)
+    fill_fcst = fcst.groupby("fuel")["price_real"].mean() * 2
+    all_st_fuels = pd.MultiIndex.from_product(
+        [fcst["st"].unique(), fcst["fuel"].unique()], names=["st", "fuel"]
+    ).to_frame(index=False)
+    fcst = all_st_fuels.merge(fcst, on=["st", "fuel"], how="left")
+    fcst["price_real"] = fcst["price_real"].fillna(fcst["fuel"].map(fill_fcst))
+    fcst["forecast_name"] = fcst_name
+    fcst_dfs.append(fcst)
 
-# for any states missing prices (defunct or absent plants),
-# fill in with arbitrary 2x the national average (to avoid looking
-# like they could be restarted cheaply)
-fill_fcst = state_fcst.groupby("fuel")["price_real"].mean() * 2
-all_st_fuels = pd.MultiIndex.from_product(
-    [state_fcst["st"].unique(), state_fcst["fuel"].unique()], names=["st", "fuel"]
-).to_frame(index=False)
-state_fcst = all_st_fuels.merge(state_fcst, on=["st", "fuel"], how="left")
-state_fcst["price_real"] = state_fcst["price_real"].fillna(
-    state_fcst["fuel"].map(fill_fcst)
-)
+state_fcst = pd.concat(fcst_dfs)
 
 # assign to regions
 region_info = gpd.read_file(
@@ -153,11 +269,12 @@ zone_fcst = state_fcst.merge(region_info[["st", "region"]]).sort_values(
 # settings_management:
 #   all_years:
 #     fuel_price_forecast:
+#       # ... leave other user stuff here
 #       hist5:
-#         # ... leave other user stuff here
 #         user_fuel_usd_year:
 #           coal: 2024
 #           naturalgas: 2024
+#           ...
 #         user_fuel_price:
 #           naturalgas:
 #             p1: 3.23
@@ -167,25 +284,47 @@ zone_fcst = state_fcst.merge(region_info[["st", "region"]]).sort_values(
 #             p1: 3.21
 #             p2: 3.21
 #             ...
-
-print(f"Adding `hist5` forecast to PowerGenome settings")
-ss = read_yaml(yaml_files["scenario_settings"])
-
-yaml_root = [
-    "settings_management",
-    "all_years",
-    "fuel_price_forecast",
-    "hist5",
-]
+#           ...
+#       y2022:
+#         user_fuel_usd_year:
+#           coal: 2024
+#           ...
+#         user_fuel_price:
+#           coal:
+#             p1: 3.23
+#             p2: 3.32
+#             ...
+#           ...
+#         ...
 
 # build dict of user fuel prices
 d = {}
+for r in zone_fcst[["forecast_name", "fuel"]].drop_duplicates().itertuples():
+    d.setdefault(r.forecast_name, {}).setdefault("user_fuel_usd_year", {})[
+        r.fuel
+    ] = base_year
 for r in zone_fcst.itertuples():
-    d.setdefault(r.fuel, {})[r.region] = r.price_real
+    d[r.forecast_name].setdefault("user_fuel_price", {}).setdefault(r.fuel, {})[
+        r.region
+    ] = r.price_real
 
-add_yaml_key(ss, yaml_root + ["user_fuel_usd_year"], {f: base_year for f in d.keys()})
-# remove any previous user_fuel_price entries, but leave other
-# elements under hist5 alone, in case there are user settings there
-delete_yaml_keys(ss, yaml_root + ["user_fuel_price"])
-add_yaml_key(ss, yaml_root + ["user_fuel_price"], d)
+fcst_names = [f"`{k}`" for k in d.keys()]
+if len(fcst_names) == 0:
+    names = "<no forecasts>"
+elif len(fcst_names) == 1:
+    names = fcst_names[0] + " forecast"
+else:
+    names = ", ".join(fcst_names[:-1]) + " and " + fcst_names[-1] + " forecasts"
+
+print(f"Adding {names} to PowerGenome settings")
+ss = read_yaml(yaml_files["scenario_settings"])
+
+yaml_root = ["settings_management", "all_years", "fuel_price_forecast"]
+for fcst_name, fcst in d.items():
+    # remove any previous forecasts with these names, but leave other
+    # forecasts alone
+    yaml_path = yaml_root + [fcst_name]
+    delete_yaml_keys(ss, yaml_path)
+    add_yaml_key(ss, yaml_path, fcst)
+
 write_yaml(ss, yaml_files["scenario_settings"])
