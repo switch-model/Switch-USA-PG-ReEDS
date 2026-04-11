bbece67381739c66591cc03907c140ebd498d672
Matthias Fripp <matthias@energyinnovation.org>
2026-04-11 08:01:15 -0700

    Improve parent directory labeling and rounding in compare_switch_inputs.py

--- compare_switch_inputs.py
+++ compare_switch_inputs.py
@@ -12,6 +12,7 @@ usage: python compare_inputs.py old_dir new_dir
 """
 
 import sys, tempfile, os, shutil, subprocess
+from pathlib import Path
 import pandas as pd, numpy as np
 
 
@@ -29,10 +30,33 @@ def round(x, n_figs):
     return rounded
 
 
-with tempfile.TemporaryDirectory() as td:
-    for out, orig in [("old", sys.argv[1]), ("new", sys.argv[2])]:
-        outdir = os.path.join(td, out)
-        os.mkdir(outdir)
+in1 = sys.argv[1]
+in2 = sys.argv[2]
+try:
+    # trim identical parts from the start and end of the paths
+    pp1 = Path(in1).parts
+    pp2 = Path(in2).parts
+    for start, (p1, p2) in enumerate(zip(pp1, pp2)):
+        if p1 != p2:
+            break
+    for end, (p1, p2) in enumerate(zip(reversed(pp1), reversed(pp2))):
+        if p1 != p2:
+            break
+    out1 = "_".join(pp1[start : len(pp1) - end])
+    out2 = "_".join(pp2[start : len(pp2) - end])
+except:
+    # flag to use dummy names
+    out1 = out2 = "bad"
+
+if out1 == out2 or out1 == "" or out2 == "":
+    out1 = in1.replace("\\", "-").replace("/", "-")
+    out2 = in2.replace("\\", "-").replace("/", "-")
+
+with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as td:
+
+    for orig, out in [(in1, out1), (in2, out2)]:
+        outdir = Path(td) / out
+        outdir.mkdir()
 
         for root, dirs, files in os.walk(orig):
             for name in dirs:
@@ -70,15 +94,18 @@ with tempfile.TemporaryDirectory() as td:
                         except TypeError:
                             # non-convertible, e.g., has non-int or inf values
                             pass
-                    # some of the original input data are rounded to 10 digits
+                    # Some of the original input data are rounded to 10 digits
                     # (maybe by processing with R?); output data may vary by
-                    # 1e-6 or so, so we round to that many digits of precision
+                    # 1e-6 or so, so we round to that many digits of precision.
+                    # We also use a float_format that converts floats to ints
+                    # where possible and drops artifacts like trailing zeroes
+                    # and small values
                     floats = df.columns[df.dtypes == "float64"]
                     df[floats] = round(df[floats], 5)
-                    df.to_csv(new_file, index=False, na_rep=".")
+                    df.to_csv(new_file, index=False, na_rep=".", float_format="%.15g")
                 else:
                     shutil.copy(old_file, new_file)
 
-    subprocess.run(["opendiff", os.path.join(td, "old"), os.path.join(td, "new")])
+    subprocess.run(["opendiff", Path(td) / out1, Path(td) / out2])
 
     input("Press Enter to finish and delete the temporary files...")
