13b1d5bb4486e02ec32088427da562d6341e6fd9
Matthias Fripp <matthias@energyinnovation.org>
2026-04-14 13:42:07 -0700

    add study_modules.reduce_reporting module to repo

---
+++ switch/study_modules/reduce_reporting.py
@@ -0,0 +1,16 @@
+# do minimal reporting to reduce disk usage for multi-year weekly models
+
+
+def define_components(m):
+    allowed_post_solve = {
+        # These modules report info that is needed even for weekly models, but some
+        # also have their own flags to reduce the output more finely
+        "study_modules.generators_core_dispatch",
+        "switch_model.generators.core.dispatch",
+        "switch_model.reporting",
+        "study_modules.unserved_load",
+        "study_modules.planning_reserves_extreme_days",
+    }
+    for module in m.get_modules():
+        if hasattr(module, "post_solve") and module.__name__ not in allowed_post_solve:
+            del module.post_solve
