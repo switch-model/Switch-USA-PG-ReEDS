a09bf89ccf6b29d097a73337fa2b29bbd67ff933
Matthias Fripp <matthias@energyinnovation.org>
2026-04-24 22:18:05 -0700

    add ra job iteration script and job definitions for aws

---
+++ switch/ra_iterate_aws.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+if anything in build-scens list:
+    remove scenario_queue
+    submit build-scens job
+    submit RA job dependent on build-scens job
+    submit ra_add_difficult_timeseries job dependent on RA job
+    submit iterate job dependent on ra_add_difficult job
+first run: either launch this script's job def or run it directly
+"""
+import json, shutil, sys
+
+import boto3
+
+
+def main():
+    with open("ra_jobs.json") as f:
+        jobs = json.load(f)
+
+    batch = boto3.client("batch", region_name=jobs["region"])
+
+    build_scens_file = jobs["ra_add_difficult_timeseries"]["ecsPropertiesOverride"][
+        "taskProperties"
+    ][0]["containers"][0]["command"][0]
+    with open(build_scens_file, "r") as f:
+        # any scenarios to run?
+        done = not f.read().strip()
+
+    if done:
+        print(f"Ending iteration; no more scenarios to run in {build_scens_file}.")
+        return
+
+    # have some scenarios to run; setup another iteration
+
+    # clear out the scenario queue so new ones will run
+    shutil.rmtree("scenario_queue")
+
+    # setup these jobs as a chain (the last one calls this script again)
+    jobs = ["solve_ce", "solve_ra", "ra_add_difficult_days", "ra_iterate_aws"]
+
+    prev_id = None
+    for job, spec in jobs.items():
+        if prev_id is None:
+            # first job
+            spec["dependsOn"] = []
+        else:
+            # each job depends on prior
+            spec["dependsOn"] = [{"jobId": prev_id, "type": "SEQUENTIAL"}]
+        resp = batch.submit_job(**spec)
+        job_id = resp["jobId"]
+        dep = f", dependent on {prev_id}" if prev_id else ""
+        print(f"Submitted job `{job}`: {job_id}{dep}")
+        prev_id = job_id
+
+
+if __name__ == "__main__" and "ipykernel" not in sys.argv[0]:
+    # running as a script, not from a jupyter environment
+    main()
